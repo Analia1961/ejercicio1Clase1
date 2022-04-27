4a5943775c9ff34ed5b71d3d499d35bce047f4c3
Analía Alvado <104107403+Analia1961@users.noreply.github.com>
2022-04-27 14:58:45 -0300

    corrección-ejercicio2_Clase1 Ejercicio de prueba online

---
+++ src/ejercicio2_Clase1.ts
@@ -0,0 +1,17 @@
+//ejercicio2_Clase1
+
+console.log("granjera1");
+console.log("avanzo y elimino");
+console.log("avanzo y elimino");
+console.log("avanzo y elimino");
+console.log("avanzo y elimino");
+console.log("giro a la izquierda");
+console.log("avanzo y elimino");
+console.log("avanzo y elimino");
+console.log("avanzo y elimino");
+console.log("avanzo y elimino");
+console.log("avanzo y elimino");
+
+/*Secuencia -Ejercicio de prueba online-
+Crear un sandbox e imprimir por consola los pasos 
+para completar las tareas de la granjera*/
